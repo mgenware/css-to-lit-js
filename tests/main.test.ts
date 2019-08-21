59a0a1c9919dd45b58c57678e8fdae331484606f
Mgen <m@mgenware.com>
2019-08-22 01:33:27 +0800

    Fix tests not working

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -2,12 +2,28 @@ import * as assert from 'assert';
 import * as tempy from 'tempy';
 import * as mfs from 'm-fs';
 import { execSync } from 'child_process';
+import rename from 'node-rename-path';
+
+function newFile(): string {
+  return tempy.file({ extension: '.css' });
+}
 
 it('Basic', async () => {
-  const tmpFile = tempy.file();
-  console.log(' tmp ', tmpFile);
+  const tmpFile = newFile();
   await mfs.writeFileAsync(tmpFile, `123\n\n"'\\\`$`);
+  console.log(tmpFile);
   execSync(`node ./dist/main.js "${tmpFile}"`);
-  const contents = await mfs.readTextFileAsync(tmpFile);
-  assert.equal(contents, `123\n\n"'\\\`\$`);
+  const contents = await mfs.readTextFileAsync(
+    rename(tmpFile, () => {
+      return {
+        ext: '.js',
+      };
+    }),
+  );
+  assert.equal(
+    contents,
+    `import {css} from 'lit-element';export default css\`123
+
+"'\\\\\\\`\\\$\`;`,
+  );
 });
